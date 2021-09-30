717a214baac358d032d3025769403ed29d6ac7c0
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-09-30 12:53:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,5 +4,15 @@ variable "access_token" {
   sensitive   = true
 }
 
+variable "project" {
+  description = "project Id"
+  type        = string
+  default  = "manifest-access-320809"
+}
+variable "bucket-name" {
+  description = "bucket name"
+  type        = string
+  default  = "bucket_1"
+}
 
 #gcloud auth print-access-token
